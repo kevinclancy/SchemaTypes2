9f834116019417c59f4b5e2ab163d3dc3f86cd84
Kevin Clancy <kclanc@gmail.com>
2026-04-21 14:13:26 -0700

    added utils module

--- lib/utils.ml
+++ lib/utils.ml
@@ -1,1 +1,10 @@
-(* Port of Utils.fs *)
+let rec all_unique_unordered_pairs (lst : 'a list) : ('a * 'a) list =
+  match lst with
+  | head :: rest ->
+    let a = List.map (fun x -> (head, x)) rest in
+    a @ all_unique_unordered_pairs rest
+  | [] -> []
+
+let fst3 (x, _, _) = x
+let snd3 (_, x, _) = x
+let thrd3 (_, _, x) = x
